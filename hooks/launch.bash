4a69c6975c0f993ddcfd71e781bab67dfdaae8b3
glass <glass.ships@outlook.com>
2019-09-06 15:21:57 -0400

    cleanup pages branch

--- hooks/launch.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-PYTHON_VER=$1
-CONFIG_FILE=$2
-if [ -e ${HOME}/notebooks/.user_setups ]; then
-    source ${HOME}/notebooks/.user_setups
-fi
-source $ROOTSYS/bin/thisroot.sh
-exec python${PYTHON_VER} -m ipykernel -f ${CONFIG_FILE}
