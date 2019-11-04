194df0d1301b2fd95a208aea56c400d012cd69cb
glass <glass.ships@outlook.com>
2019-11-04 16:37:49 -0700

    update tutorial structure

--- hooks/post-hook.sh
+++ hooks/post-hook.sh
@@ -1,29 +1,7 @@
 ## Create or clear Tutorials dir, copy examples and tutorials
-
-if [ ! -d "$HOME/notebooks/Tutorials" ]; then
-  mkdir -p $HOME/notebooks/Tutorials
-fi
-
 chmod -R 755 $HOME/notebooks/Tutorials
-
-rm -rf $HOME/notebooks/Tutorials/*
-
-mkdir $HOME/notebooks/Tutorials/pyCAP 
-ln -s /opt/pyCAP/examples/* $HOME/notebooks/Tutorials/pyCAP/
-
-mkdir $HOME/notebooks/Tutorials/scdmsPyTools 
-ln -s /opt/scdmsPyTools/demo/* $HOME/notebooks/Tutorials/scdmsPyTools/
-
-mkdir $HOME/notebooks/Tutorials/Analysis
-ln -s /opt/tutorials/tutorial1_ivcurves_tc.ipynb $HOME/notebooks/Tutorials/Analysis/'Tutorial 1 - IV Curves (TC).ipynb'
-ln -s /opt/tutorials/animal_circuit.png $HOME/notebooks/Tutorials/Analysis/animal_circuit.png
-
-mkdir $HOME/notebooks/Tutorials/Introduction 
-ln -s /opt/tutorials/JupyterDemo-Jan01.ipynb $HOME/notebooks/Tutorials/Introduction/'Intro to JupyterLab'.ipynb
-ln -s /opt/tutorials/2019-01-06_111527.jpg $HOME/notebooks/Tutorials/Introduction/
-ln -s /opt/tutorials/AnimalDataIO.py $HOME/notebooks/Tutorials/Introduction/
-ln -s /opt/tutorials/logout.png $HOME/notebooks/Tutorials/Introduction/
-
+rm -rf $HOME/notebooks/Tutorials
+ln -s /opt/tutorials/Tutorials $HOME/notebooks/Tutorials
 chmod -R 555 $HOME/notebooks/Tutorials
 
 ## Customize bash env
