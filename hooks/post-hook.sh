f5364e26fb7022fe87a142f607fd97729e7acd31
glass <glass.ships@outlook.com>
2019-11-25 16:04:47 -0700

    fix broken link to pycap/scdmspytools tutorials

--- hooks/post-hook.sh
+++ hooks/post-hook.sh
@@ -1,13 +1,17 @@
 ## Create or clear Tutorials dir, copy examples and tutorials
 chmod -R 755 $HOME/notebooks/Tutorials
+
 rm -rf $HOME/notebooks/Tutorials
-ln -s /opt/tutorials/Tutorials $HOME/notebooks/Tutorials
+mkdir $HOME/notebooks/Tutorials
 
-mkdir $HOME/notebooks/Tutorials/pyCAP 
-ln -s /opt/pyCAP/examples/* $HOME/notebooks/Tutorials/pyCAP/
+ln -s /opt/Analysis/tutorials/Tutorials/* $HOME/notebooks/Tutorials/
 
-mkdir $HOME/notebooks/Tutorials/scdmsPyTools 
-ln -s /opt/scdmsPyTools/demo/* $HOME/notebooks/Tutorials/scdmsPyTools/
+mkdir -p $HOME/notebooks/Tutorials/pyCAP
+ln -s /opt/Analysis/pyCAP/examples/* $HOME/notebooks/Tutorials/pyCAP/
+
+mkdir -p $HOME/notebooks/Tutorials/scdmsPyTools
+ln -s /opt/Analysis/scdmsPyTools/demo/* $HOME/notebooks/Tutorials/scdmsPyTools/
+
 chmod -R 555 $HOME/notebooks/Tutorials
 
 ## Customize bash env
